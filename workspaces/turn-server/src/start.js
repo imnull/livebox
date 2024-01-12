50eb538d43c3fab32ae9c820d971616945c46742
marvin <imnull@outlook.com>
2024-01-12 17:40:38 +0800

    dev: turn listeningIps

--- workspaces/turn-server/src/start.js
+++ workspaces/turn-server/src/start.js
@@ -1,7 +1,11 @@
 var Turn = require('node-turn');
 var server = new Turn({
     listeningPort: 3478,
-    // realm: 'mkjs.net',
+    listeningIps: [
+        '8.130.9.126',
+        '172.22.137.20',
+    ],
+    realm: 'turn.mkjs.net',
     // set options
     authMech: 'long-term',
     // authMech: 'none',
