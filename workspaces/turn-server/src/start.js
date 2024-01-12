01eda8bc82b14efebac744faf83db826484cc060
marvin <imnull@outlook.com>
2024-01-12 18:15:29 +0800

    dev: turn-server config

--- workspaces/turn-server/src/start.js
+++ workspaces/turn-server/src/start.js
@@ -4,6 +4,8 @@ const config = require('./config.json')
 const server = new Turn({
     listeningPort: 3478,
     listeningIps: config.listeningIps,
+    relayIps: config.relayIps,
+    externalIps: config.externalIps,
     realm: config.realm,
     // set options
     authMech: config.authMech,
